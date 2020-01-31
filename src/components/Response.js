f2463faa49903729c16f48f747ae14df8b4daaa5
Casey Martell <drmartell@gmail.com>
2020-01-30 17:00:25 -0800

    resolves empty object display

--- src/components/Response.js
+++ src/components/Response.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactJSON from 'react-json-view';
 
-const Response = ({ responseBody }) => (
-  <pre>{JSON.stringify(responseBody)}</pre>
-);
+const Response = ({ responseBody }) => {
+  return (Object.keys(responseBody).length !== 0 && responseBody.constructor === Object) ?
+    <ReactJSON src={responseBody}/> :
+    '';
+};
 
 Response.propTypes = {
-  responseBody: PropTypes.string
+  responseBody: PropTypes.object
 };
 
 export default Response;
